14dd67bcd13ebb7b490091e451171baf834218d9
OkashaAbdalla <okashaabdall44a@gmail.com>
2026-01-13 12:04:00 +0100

    Minor changes

--- snaplink-backend/src/index.js
+++ snaplink-backend/src/index.js
@@ -24,11 +24,9 @@ app.use(
     origin: [
       process.env.FRONTEND_URL || "http://localhost:5173",
       "https://linksnap-okasha.vercel.app",
-      "https://linksnap-okash.vercel.app",
       "https://linksnap-eta.vercel.app",
       "http://localhost:5173",
       "http://localhost:5174",
-      "http://localhost:5175",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174"
     ],
